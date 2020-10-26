4d26dc7e808b5fe43be71f475116e7dfb4803927
4ndrenobr3 <nobre4ndr3@gmail.com>
2020-10-26 05:19:42 -0300

    Instalado o twig e criado as views.

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -1,1 +1,30 @@
 <?php
+namespace SearchApp\Controllers;
+
+use SearchApp\Models\Connection;
+use SearchApp\Models\Customer;
+
+class HomeController
+{
+    protected function view($view, $params = [])
+    {
+        $loader = new \Twig\Loader\FilesystemLoader('../resources/views/');
+
+        $twig = new \Twig\Environment($loader, []);
+
+        echo $twig->render($view . '.php.twig', $params);
+    }
+
+    public function index()
+    {
+        //return $this->view('index');
+        $cliente = new Customer(Connection::getInstance());
+        var_dump($cliente->find());
+    }
+
+    public function search()
+    {
+        $customer = filter_input(INPUT_POST, 'customer_name', FILTER_SANITIZE_STRING);
+        return  $customer . 'Este esta vindo pelo controller';
+    }
+}
