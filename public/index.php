2da2d8242127099b5122b82bdc2ac465a9813ef2
4ndrenobr3 <nobre4ndr3@gmail.com>
2020-10-25 22:41:31 -0300

    Definição das rotas no public/index.php e linkado autoload no bootstrap.

--- public/index.php
+++ public/index.php
@@ -1,1 +1,24 @@
 <?php
+require_once '../bootstrap.php';
+
+$url = substr($_SERVER['REQUEST_URI'], 1);
+$url = explode('/', $url);
+
+$controller = isset($url[0]) && $url[0] ? $url[0] : 'home';
+$action     = isset($url[1]) && $url[1] ? $url[1] : 'index';
+$param      = isset($url[2]) && $url[2] ? $url[2] : null;
+
+if(!class_exists($controller = "SearchApp\Controllers\\" . ucfirst($controller) . 'Controller')) {
+
+    echo '404- Página não encontrada!';
+    die;
+}
+
+if(!method_exists($controller, $action)) {
+    $action = 'index';
+    $param  = $url[1];
+}
+
+$response = call_user_func_array([new $controller, $action], [$param]);
+
+print $response;
